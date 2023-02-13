22f231f182d491f30c6785068a0a21773420e394
wallentx <william.allentx@gmail.com>
2023-02-13 04:31:29 -0600

    Optimized script

--- print-sysinfo.sh
+++ print-sysinfo.sh
@@ -41,18 +41,24 @@ function getInfo() {
   SYSNAME=$(hostname)
   OSNAME=$(hostnamectl | awk -F ': ' '/Operating System/ {print $2}')
   OSKERNEL=$(uname -r)
-  VENDOR=($(hostnamectl | grep 'Hardware' | sed 's/.*: //g'))
-  CPUCOUNT=$(lscpu | grep 'Socket(s):' | awk -F: '{print $2}' | xargs)
+  VENDOR=($(hostnamectl | awk -F ': ' '/Hardware/ {print $2}'))
+  CPUCOUNT=$(lscpu | awk '/Socket\(s\)/ {print $2"x"}')
   CPUMODEL=$(lscpu | grep '^Model name:' | awk -F: '{print $2}' | xargs)
-  CPUCORES=$(lscpu | grep 'Core(s) per socket:' | awk -F: '{print $2}' | xargs)
-  CPUTHREADS=$(lscpu | grep '^CPU(s)' | awk -F: '{print $2}' | xargs)
-  GPUNAME=$(nvidia-smi --query-gpu=name --format=csv,noheader)
-  GPUDRIVER=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
-  GPUPCIEGEN=$(nvidia-smi --query-gpu=pcie.link.gen.current --format=csv,noheader)
-  GPUPCIEWIDTH=$(nvidia-smi --query-gpu=pcie.link.width.current --format=csv,noheader)
+  CPUCORES=$(lscpu | awk '/Core\(s\) per socket/ {print $4}')
+  CPUTHREADS=$(lscpu | awk '/^CPU\(s\)/ {print $2}')
+
+  GPUINFO=$(nvidia-smi --query-gpu=name,driver_version,pcie.link.gen.current,pcie.link.width.current --format=csv,noheader)
+  IFS=',' read -r GPUNAME GPUDRIVER GPUPCIEGEN GPUPCIEWIDTH <<< "$GPUINFO"
+#  GPUNAME=$(nvidia-smi --query-gpu=name --format=csv,noheader)
+#  GPUDRIVER=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
+#  GPUPCIEGEN=$(nvidia-smi --query-gpu=pcie.link.gen.current --format=csv,noheader)
+#  GPUPCIEWIDTH=$(nvidia-smi --query-gpu=pcie.link.width.current --format=csv,noheader)
+
+  MEMINFO=$(dmidecode -t memory)
   MEMTOTAL=$(grep 'MemTotal' /proc/meminfo | awk '{print $2/1024/1024 "GB"}')
-  MEMDIMMS=$(dmidecode -t memory | grep -i size | grep -v 'No' | awk '{ print $2$3}' | sort -rn | uniq -c | awk '{ printf("%dx%s", $1, $2) }')
-  MEMSPEED=$(dmidecode -t memory | grep "Speed:" | head -1 | awk '{ print $2 "MT/s" }')
+  MEMDIMMS=$(echo "$MEMINFO" | grep -i size | grep -v 'No' | awk '{ print $2$3}' | sort -rn | uniq -c | awk '{ printf("%dx%s", $1, $2) }')
+  MEMTYPE=$(echo "$MEMINFO" | awk '/Memory Device/,/^$/ {if ($0 ~ /Type:/) {print $2}}' | uniq)
+  MEMSPEED=$(echo "$MEMINFO" | grep "Speed:" | head -1 | awk '{ print $2 "MT/s" }')
 
   # Clear loading bar
   echo -ne "\r"
@@ -63,15 +69,21 @@ function getInfo() {
   # System Vendor/Model
   echo -e "SystemVendor/Model=\"${VENDOR[*]}\""
   # CPU
-  echo -e 'CPU="'$CPUCOUNT"x $CPUMODEL (C:$CPUCORES|T:$CPUTHREADS)\""
+  echo -e "CPU=\"$CPUCOUNT $CPUMODEL (C:$CPUCORES|T:$CPUTHREADS)\""
   # GPU
-  echo -e "GPU=\"$GPUNAME\""
+  printf 'GPU="%s"\n' "$GPUNAME"
   # NVIDIA Driver
-  echo -e "DriverVersion=\"$GPUDRIVER\""
+  printf 'DriverVersion="%s"\n' "${GPUDRIVER#" "}"
   # PCIe link
-  echo -e 'PCIeLink="PCIe '$GPUPCIEGEN"x"$GPUPCIEWIDTH"\""
+  printf 'PCIeLink="PCIe %sx%s"\n' "${GPUPCIEGEN#" "}" "${GPUPCIEWIDTH#" "}"
+  ## GPU
+  ##echo -e "GPU=\"$GPUNAME\""
+  ## NVIDIA Driver
+  ##echo -e "DriverVersion=\"$GPUDRIVER\""
+  ## PCIe link
+  ##echo -e 'PCIeLink="PCIe '$GPUPCIEGEN"x"$GPUPCIEWIDTH"\""
   # DRAM (Size, Type, MHz, etc)
-  echo -e "DRAM=\"$MEMTOTAL, $MEMDIMMS, $MEMSPEED\""
+  echo -e "DRAM=\"$MEMTOTAL, $MEMDIMMS $MEMTYPE, $MEMSPEED\""
 }
 
 function printInfo() {
