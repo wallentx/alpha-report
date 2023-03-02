c66966cf57696d55cdfa71326231966a180b8042
wallentx <william.allentx@gmail.com>
2023-03-02 02:04:14 -0600

    Adding alphabit and some fixups

--- print-sysinfo.sh
+++ print-sysinfo.sh
@@ -9,33 +9,25 @@ fi
 TMPOUT=$(mktemp /tmp/tmpXXXXXXXXXX)
 
 function shutdown() {
-  tput cnorm
-  rm $TMPOUT
+  #tput cnorm
+  rm "$TMPOUT"
 }
 trap shutdown EXIT
 
-function cursorBack() {
-  echo -en "\033[$1D"
-}
+while getopts ":s" option; do
+  case $option in
+  s)
+    SOURCED=true
+    ;;
+  \?)
+    echo "Invalid option."
+    exit
+    ;;
+  esac
+done
+shift "$((OPTIND - 1))"
 
-function spinner() {
-  local LC_CTYPE=C
-  local LC_ALL=en_US.utf-8
-  tput civis
-  local CL="\e[2K"
-  local spin="⣷⣯⣟⡿⢿⣻⣽⣾"
-  local pid=$(jobs -p)
-  local charwidth=1
-  while kill -0 $pid 2>/dev/null; do
-    local i=$(((i + charwidth) % ${#spin}))
-    printf "%s" "$(tput setaf 2)${spin:i:charwidth}$(tput sgr0)"
-    cursorBack 1
-    sleep .1
-  done
-  printf "${CL}"
-  tput cnorm
-  wait $(jobs -p)
-}
+source ./extra/spinner
 
 function getInfo() {
   SYSNAME=$(hostname)
@@ -45,46 +37,49 @@ function getInfo() {
   CPUCOUNT=$(lscpu | awk '/Socket\(s\)/ {print $2"x"}')
   CPUMODEL=$(lscpu | grep '^Model name:' | awk -F: '{print $2}' | xargs)
   CPUCORES=$(lscpu | awk '/Core\(s\) per socket/ {print $4}')
-  CPUTHREADS=$(lscpu | awk '/^CPU\(s\)/ {print $2}')
+  CPUTHREADS=$(lscpu | awk '/^CPU\(s\):/ {print $2}')
   GPUINFO=$(nvidia-smi --query-gpu=name,driver_version,pcie.link.gen.current,pcie.link.width.current --format=csv,noheader)
-  IFS=',' read -r GPUNAME GPUDRIVER GPUPCIEGEN GPUPCIEWIDTH <<< "$GPUINFO"
-  GPUQINFO=( $(nvidia-smi -q | grep 'Board Part Number\|Product Architecture' | awk '{print $NF}') )
-  IFS=' ' read -r GPUBOARD GPUARCH <<< "${GPUQINFO[*]}"
-  MEMINFO=$(dmidecode -t memory)
+  IFS=',' read -r GPUNAME GPUDRIVER GPUPCIEGEN GPUPCIEWIDTH <<<"$GPUINFO"
+  GPUQINFO=($(nvidia-smi -q | grep 'Board Part Number\|Product Architecture' | awk '{print $NF}'))
+  IFS=' ' read -r GPUBOARD GPUARCH <<<"${GPUQINFO[*]}"
+  MEMINFO=$(dmidecode -qt memory)
   MEMTOTAL=$(grep 'MemTotal' /proc/meminfo | awk '{print $2/1024/1024 "GB"}')
   MEMDIMMS=$(echo "$MEMINFO" | grep -i size | grep -v 'No' | awk '{ print $2$3}' | sort -rn | uniq -c | awk '{ printf("%dx%s", $1, $2) }')
   MEMTYPE=$(echo "$MEMINFO" | awk '/Memory Device/,/^$/ {if ($0 ~ /Type:/) {print $2}}' | uniq)
   MEMSPEED=$(echo "$MEMINFO" | grep "Speed:" | head -1 | awk '{ print $2 "MT/s" }')
 
-  # Clear loading bar
-  echo -ne "\r"
   # System Name
-  echo -e "SystemName=\"$SYSNAME\""
+  printf 'SYSTEM_NAME="%s"\n' "$SYSNAME"
   # OS
-  echo -e "OS=\"${OSNAME#*: } $OSKERNEL\""
+  printf 'OS="%s"\n' "${OSNAME#*: } $OSKERNEL"
   # System Vendor/Model
-  echo -e "SystemVendor/Model=\"${VENDOR[*]}\""
+  echo -e "SYSVENDOR_MODEL=\"${VENDOR[*]}\""
   # CPU
   echo -e "CPU=\"$CPUCOUNT $CPUMODEL (C:$CPUCORES|T:$CPUTHREADS)\""
   # GPU Name
   printf 'GPU="%s"\n' "$GPUNAME"
   # GPU SKU
-  printf 'GPUSKU="%s"\n' "$GPUBOARD"
+  printf 'GPU_SKU="%s"\n' "$GPUBOARD"
   # GPU Arch
-  printf 'GPUArch="%s"\n' "$GPUARCH"
+  printf 'GPU_ARCH="%s"\n' "$GPUARCH"
   # NVIDIA Driver
-  printf 'DriverVersion="%s"\n' "${GPUDRIVER#" "}"
+  printf 'GPU_DRIVER="%s"\n' "${GPUDRIVER#" "}"
   # PCIe link
-  printf 'PCIeLink="PCIe %sx%s"\n' "${GPUPCIEGEN#" "}" "${GPUPCIEWIDTH#" "}"
+  printf 'PCIE_LINK="PCIe %sx%s"\n' "${GPUPCIEGEN#" "}" "${GPUPCIEWIDTH#" "}"
   # DRAM (Size, Type, MHz, etc)
-  echo -e "DRAM=\"$MEMTOTAL, $MEMDIMMS $MEMTYPE, $MEMSPEED\""
+  printf 'DRAM="%s"' "$MEMTOTAL, $MEMDIMMS $MEMTYPE, $MEMSPEED"
 }
 
 function printInfo() {
-  getInfo >$TMPOUT &
-  spinner
-  cat $TMPOUT
+  getInfo >"$TMPOUT" &
+  _spinner
+  echo -ne "\r"
+  cat "$TMPOUT"
 }
 
-echo -n "Gathering system info..."
-printInfo
+if [[ $SOURCED == true ]]; then
+  getInfo
+else
+  echo -n "Gathering system info..."
+  printInfo
+fi
